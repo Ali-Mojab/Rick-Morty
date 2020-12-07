2ad5249eb4182754ef4d1b522b4d060023325c0b
Ali Mojab <its.ali.mojab@gmail.com>
2020-12-08 03:04:17 +0330

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+
+let rick = document.getElementById('rick');
+
+
+let rosegoldColor = () =>{ 
+    rick.src='/img/60.png';
+    background.style.backgroundColor = 'deepskyblue';
+    title.style.color = 'deepskyblue';
+    span1.style.backgroundColor = 'deepskyblue';
+    span2.style.backgroundColor = 'deepskyblue';
+}
+
+let coralColor = () =>{
+    rick.src='/img/10.png';
+    background.style.backgroundColor = '#f85241';
+    title.style.color = '#f85241';
+    span1.style.backgroundColor = '#f85241';
+    span2.style.backgroundColor = '#f85241';
+}
+
+let fuschiaColor =() =>{
+    rick.src='/img/12.png';
+    background.style.backgroundColor = '#924df4';
+    title.style.color = '#924df4';
+    span1.style.backgroundColor = '#924df4';
+    span2.style.backgroundColor = '#924df4';
+}
+
+let goldColor = () => {
+    rick.src='/img/11.png';
+    background.style.backgroundColor = '#e0bc4d';
+    title.style.color = '#e0bc4d';
+    span1.style.backgroundColor = '#e0bc4d';
+    span2.style.backgroundColor = '#e0bc4d';
+}
+
+let greenColor = () => {
+    rick.src='/img/32.png';
+    background.style.backgroundColor = '#52e04d';
+    title.style.color = '#52e04d';
+    span1.style.backgroundColor = '#52e04d';
+    span2.style.backgroundColor = '#52e04d';
+}
+
+let darkyellowColor = () => {
+    rick.src='/img/34.png';
+    background.style.backgroundColor = '#d8a028';
+    title.style.color = '#d8a028';
+    span1.style.backgroundColor = '#d8a028';
+    span2.style.backgroundColor = '#d8a028';
+}
+
+let orangeColor = () => {
+    rick.src='/img/6.png';
+    background.style.backgroundColor = '#f3872e';
+    title.style.color = '#f3872e';
+    span1.style.backgroundColor = '#f3872e';
+    span2.style.backgroundColor = '#f3872e';
+}
+
+let blueColor = () => {
+    rick.src='/img/4.png';
+    background.style.backgroundColor = '#4dcae0';
+    title.style.color = '#4dcae0';
+    span1.style.backgroundColor = '#4dcae0';
+    span2.style.backgroundColor = '#4dcae0';
+}
+
+let yellowColor = () => {
+    rick.src='/img/7.png';
+    background.style.backgroundColor = '#c7c42f';
+    title.style.color = '#c7c42f';
+    span1.style.backgroundColor = '#c7c42f';
+    span2.style.backgroundColor = '#c7c42f';
+}
+
+// let changeColor = (imgUrl , color) =>{
+//     rick.src = imgUrl;
+//     background.style.backgroundColor = color;
+//     title.style.color = color;
+//     span1.style.backgroundColor = color;
+//     span2.style.backgroundColor = color;
+// }
+
+// changeColor("/img/7.png" , '#c7c42f' );
+// changeColor("/img/7.png" , '#c7c42f' );
